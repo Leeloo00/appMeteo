33429b5fa99d7e2dcf344714c94e248e9b5b2be6
ELia Akeb <lilouachab@gmail.com>
2022-11-10 14:47:35 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const APIKEY = 'a72dac462c18aa2d30ef5807a95ff440';
+
+
+/*Appel à l'API avec ville en paramère de fonction*/
+let apiCall = function(city){
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&appid=${APIKEY}&units=metric`; 
+
+    // J'éxute ma méthode fetch . then pour récuperer la response je la traite pour l'avoir en json uqand je l'ai formaté en json je refait un .then pour récupérer la nouvelle promesse je récupère les data je les affiche premièrement en console.log
+    fetch(url).then((response) => 
+    response.json().then((data) => {
+        console.log(data)
+        document.getElementById('name').innerHTML = data.name;
+        document.getElementById('temps').innerHTML = data.main.temp + '°c';
+        document.getElementById('weather').innerHTML = data.weather[0].description;
+        document.getElementById('min').innerHTML = "Min : "+ data.main.temp_min + '°c';
+        document.getElementById('max').innerHTML = "Max : "+ data.main.temp_max + '°c';
+        document.getElementById('wind').innerHTML = data.wind.gust + 'km/h';
+        document.getElementById('humidity').innerHTML = data.main.humidity + '%';
+
+        var img = document.createElement('img');
+        img.src= 'http://openweathermap.org/img/wn/'+ data.weather[0].icon +'.png';
+        document.getElementById('icon').appendChild(img);
+        img.style.width = '100px';
+
+    })
+
+
+).catch(err => console.log('Erreur :' + err));
+
+}
+
+/*Ecouteur d'évènement sur la soumission du formulaire*/
+document.querySelector('form').addEventListener('submit', function(e){
+    e.preventDefault();
+
+    document.getElementById('icon').firstElementChild.remove();
+
+    let city = document.querySelector('#cityname').value;
+    apiCall(city);
+})
+
+// Appel par défaut au chargement de la page
+apiCall('Paris');
